15ebe91babbc2c6148c6403980dfccad95a59b36
jakcw <jackwestcott2@gmail.com>
2023-02-23 18:55:32 +1100

    some changes

--- src/board.py
+++ src/board.py
@@ -12,9 +12,40 @@ class Board:
         self.pieces = []
 
         for x in range(7):
-            self.pieces.append(Pawn('w', 'r', P(x, 1)))
+            self.pieces.append(Pawn('w', 'p', P(x, 1)))
             self.pieces.append(Pawn('b', 'p', P(x, 6)))
 
 
+        # rather than appending each piece we can just extend the array
+        self.pieces.extend(
+                [
+                    Rook('w', 'r', P(0, 0)),
+                    Knight('w', 'n', P(1, 0)),
+                    Bishop('w', 'b',  P(2, 0)),
+                    Queen('w', 'q',  P(3, 0)),
+                    King('w', 'k',  P(4, 0)),
+                    Bishop('w', 'b',  P(5, 0)),
+                    Knight('w', 'n',  P(6, 0)),
+                    Rook('w', 'r',  P(7, 0)),
+                ])
+        
+        self.pieces.extend(
+                [
+                    Rook('b', 'r', P(0, 7)),
+                    Knight('b', 'n', P(1, 7)),
+                    Bishop('b', 'b',  P(2, 7)),
+                    Queen('b', 'q',  P(3, 7)),
+                    King('b', 'k',  P(4, 7)),
+                    Bishop('b', 'b',  P(5, 7)),
+                    Knight('b', 'n',  P(6, 7)),
+                    Rook('b', 'r',  P(7, 7)),
+                ])
+                 
+    # currently, each piece is just sitting in the pieces array with information on its position; outputting it
+    # to the console will not display it in chess board format
+    def display_board(self):
+        for piece in self.pieces:
+            
 
-                    
+board = Board()
+board.display_board()
--- src/pieces.py
+++ src/pieces.py
@@ -3,7 +3,7 @@ from position import Position as P
 class Pawn:
 
     # hopefully use unicode for pieces at some point, so each init will have a default symbol
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
         self.position = position
@@ -15,50 +15,52 @@ class Pawn:
     def legal_move(self):
         pass
 
-    def move(self, new: Position):
-        if self.legal_move():
-            self.position = new
 
 
 class King:
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
+        self.position = position
     
     def __repr__(self):
         return self.side + self.symbol
         
 class Queen:
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
+        self.position = position
         self.val = 9
 
     def __repr__(self):
         return self.side + self.symbol
 
 class Rook:
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
+        self.position = position
         self.val = 5
 
     def __repr__(self):
         return self.side + self.symbol
 
 class Bishop:
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
+        self.position = position
         self.val = 3
 
     def __repr__(self):
         return self.side + self.symbol
 
 class Knight:
-    def __init__(self, side: str, symbol: str, position: Position):
+    def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
+        self.position = position
         self.val = 3
 
     def __repr__(self):
