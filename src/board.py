cea437491607d758ec8d2f9376b92768e8f2da63
Jack <98250548+jakcw@users.noreply.github.com>
2023-02-25 20:50:29 +1100

    Update board.py

--- src/board.py
+++ src/board.py
@@ -9,42 +9,61 @@ from position import Position as P
 class Board:
     def __init__(self):
 
-        self.board = [['0' for x in range(8)] for y in range(8)]
-        self.pieces = []
+        self.board = [["x" for x in range(8)] for y in range(8)]
+        self.check: bool = False
 
-        # this way each piece in the board array contains position information rather than just an array index
+        # Each piece contains positional information as well as a regular array index
         for x in range(8):
-            self.board[1][x] = Pawn('b', 'p', P(x, 1))
-            self.board[6][x] = Pawn('b', 'p', P(x, 6))
-            
+            self.board[1][x] = Pawn('b', '\u265F', P(x, 1))
+            self.board[6][x] = Pawn('w', '\u2659', P(x, 6))
+
+        # Pieces using chess unicode for clarity in terminal
+        self.board[0][0] = Rook('b', "\u2656", P(0, 0))
+        self.board[0][1] = Knight('b', "\u2658", P(1, 0))
+        self.board[0][2] = Bishop('b', "\u2657", P(2, 0))
+        self.board[0][3] = Queen('b',  "\u2655", P(3, 0))
+        self.board[0][4] = King('b', "\u2654", P(4, 0))
+        self.board[0][5] = Bishop('b', "\u2657", P(5, 0))
+        self.board[0][6] = Knight('b', "\u2658", P(6, 0))
+        self.board[0][7] = Rook('b', "\u2656", P(7, 0))
 
 
-        # rather than appending each piece we can just extend the array
-        self.pieces.extend(
-                [
-                    Rook('w', 'r', P(0, 0)),
-                    Knight('w', 'n', P(1, 0)),
-                    Bishop('w', 'b',  P(2, 0)),
-                    Queen('w', 'q',  P(3, 0)),
-                    King('w', 'k',  P(4, 0)),
-                    Bishop('w', 'b',  P(5, 0)),
-                    Knight('w', 'n',  P(6, 0)),
-                    Rook('w', 'r',  P(7, 0)),
-                ])
+        self.board[7][0] = Rook('w', "\u265C", P(0, 0))
+        self.board[7][1] = Knight('w', "\u265E", P(1, 0))
+        self.board[7][2] = Bishop('w', "\u265D", P(2, 0))
+        self.board[7][3] = Queen('w', "\u265B", P(3, 0))
+        self.board[7][4] = King('w', "\u265A", P(4, 0))
+        self.board[7][5] = Bishop('w', "\u265D", P(5, 0))
+        self.board[7][6] = Knight('w', "\u265E", P(6, 0))
+        self.board[7][7] = Rook('w', "\u265C", P(7, 0))
+
+
+    def capture(self):
+        # if new position of piece is not empty, returns true
+        if self.board[x][y] != "x":
+            return True
+        return False
+
+
+    # makeshift board display for now
+    def display_board(self):
+
+        ranks = '12345678'
+        files = 'abcdefgh'
+
+        for i in range(8):
+            print()
+            print(ranks[i], end=" ")
+            for j in range(8):
+                print(self.board[i][j], end=" ")
+
+        print()
+        print(" ", end=" ")
         
-        self.pieces.extend(
-                [
-                    Rook('b', 'r', P(0, 7)),
-                    Knight('b', 'n', P(1, 7)),
-                    Bishop('b', 'b',  P(2, 7)),
-                    Queen('b', 'q',  P(3, 7)),
-                    King('b', 'k',  P(4, 7)),
-                    Bishop('b', 'b',  P(5, 7)),
-                    Knight('b', 'n',  P(6, 7)),
-                    Rook('b', 'r',  P(7, 7)),
-                ])
+
+        for i in range(len(files)):
+            print(files[i], end=" ")
+        print()
 
 
 
-
-board = Board()
