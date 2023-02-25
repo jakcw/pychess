135434f3c45a4388f6703d37bc0d3ffe64fed853
Jack <98250548+jakcw@users.noreply.github.com>
2023-02-25 20:50:53 +1100

    Update pieces.py

--- src/pieces.py
+++ src/pieces.py
@@ -1,5 +1,9 @@
 from position import Position as P
 
+
+class MoveError(Exception):
+    pass
+
 class Pawn:
     # hopefully use unicode for pieces at some point, so each init will have a default symbol
     def __init__(self, side: str, symbol: str, position):
@@ -7,12 +11,25 @@ class Pawn:
         self.side = side
         self.position = position
         self.val = 1
+        self.moves_made = 0
 
     def __repr__(self):
-        return self.side + self.symbol
+        return self.symbol
 
-    def legal_move(self):
-        pass
+    def move(self):
+
+        # if pawn has made no moves, can move 2 positions vertically
+        if self.side == 'w':
+            if self.moves_made == 0:
+                move = P(0, 2)
+            else:
+                move = P(0, 1)
+        else:
+            if self.moves_made == 0:
+                move = P(0, -2)
+            else:
+                move = P(0, -1)
+
 
 
 class King:
@@ -22,7 +39,12 @@ class King:
         self.position = position
     
     def __repr__(self):
-        return self.side + self.symbol
+        return self.symbol
+
+    
+    def move(self, new_position):
+        self.position = new_position
+    
         
 class Queen:
     def __init__(self, side: str, symbol: str, position):
@@ -32,18 +54,29 @@ class Queen:
         self.val = 9
 
     def __repr__(self):
-        return self.side + self.symbol
+        return self.symbol 
+        
+    def move(self, new_position):
+        self.position = new_position
 
+    
 class Rook:
     def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
         self.side = side
         self.position = position
+        self.movement = [P(0, 1), P(0, -1), P(1, 0), P(-1, 0)]
         self.val = 5
 
     def __repr__(self):
-        return self.side + self.symbol
+        return self.symbol
 
+    
+    def move(self, num_squares, direction):
+        """Movement for rooks will be 1 or -1 in each direction, can then multiply that
+        by the number of squares moved and add it to current position"""
+        self.position += (num_squares * self.movement[direction])
+        
 class Bishop:
     def __init__(self, side: str, symbol: str, position):
         self.symbol = symbol
@@ -52,7 +85,10 @@ class Bishop:
         self.val = 3
 
     def __repr__(self):
-        return self.side + self.symbol
+        return  self.symbol
+
+    def move(self, new_position):
+        self.position = new_position
 
 class Knight:
     def __init__(self, side: str, symbol: str, position):
@@ -62,6 +98,9 @@ class Knight:
         self.val = 3
 
     def __repr__(self):
-        return self.side + self.symbol
+        return self.symbol
+    
+    def move(self, new_position):
+        self.position = new_position
 
 
